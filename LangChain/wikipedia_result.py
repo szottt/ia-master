c91d0bc9f8b8b2d7d46fe2aff4144333468c5125
igor szot <szottt@hotmail.com>
2025-12-15 20:27:15 -0300

    :sparkles: feat: adicionando os novos codigos das aulas sobre langchain

---
+++ LangChain/wikipedia_result.py
@@ -0,0 +1,12 @@
+from langchain_community.tools import WikipediaQueryRun
+
+from langchain_community.utilities import WikipediaAPIWrapper
+
+wikipedia = WikipediaQueryRun(
+    api_wrapper=WikipediaAPIWrapper(
+        lang='pt'
+    )
+)
+
+wikipedia_results = wikipedia.run("Alan Turing")
+print(wikipedia_results)
